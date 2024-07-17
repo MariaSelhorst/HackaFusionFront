9382b4f49543c68072165005af2649ccc35a7396
Mariasina <carolbeveer@gmail.com>
2024-07-17 13:21:00 -0300

    Checkpoint

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles/global.css"
 import Login from './pages/Login'
 import Home from './pages/ClassManagement'
 import ClassCalendar from './pages/ClassCalendar'
+import ListClasses from './pages/ListClasses'
 
 
 const router = createBrowserRouter([
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
   {
     path: "/class/calendar",
     element: <ClassCalendar/>
+  },
+  {
+    path: "/class/list",
+    element: <ListClasses/>
   }
 ])
 
