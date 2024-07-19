9bf0e5cf35e849d34dadeeb4400c54d94b6026ff
JoviOakz <joao.mateussdc@gmail.com>
2024-07-19 14:16:04 -0300

    feat:skills page

--- src/pages/instructor/InstructorRating/index.tsx
+++ src/pages/instructor/InstructorRating/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Typography, Card, CardContent, IconButton, Grid, Box } from "@mui/material";
+import { Typography, Card, CardContent, IconButton, Grid } from "@mui/material";
 import Sidebar from "../../../components/Sidebar";
 import AddRatingModal from "./components/AddRatingModal";
 import EditIcon from '@mui/icons-material/Edit';
@@ -22,38 +22,31 @@ export default function InstructorRating() {
 
     return (
         <Sidebar name="Habilidades">
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mb: 2 }}>
-                <Typography variant="h3">Habilidades</Typography>
-                <AddRatingModal skills={skills} setSkills={setSkills} />
-            </Box>
-            {skills.length === 0 ? (
-                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
-                    <img src="/imgbackground.svg" alt="Imagem quando não há habilidades" style={{ maxWidth: '100%', maxHeight: '100%', opacity: 0.5 }} />
-                </Box>
-            ) : (
-                <Grid container spacing={2}>
-                    {skills.map((skill, index) => (
-                        <Grid item xs={12} sm={6} md={4} key={index}>
-                            <Card>
-                                <CardContent>
-                                    <Typography variant="h5" component="div">{skill.name}</Typography>
-                                    <Typography variant="body2" color="text.secondary">
+            <Typography variant="h3">Habilidades</Typography>
+            <AddRatingModal skills={skills} setSkills={setSkills} />
+            <Grid container spacing={2}>
+                {skills.map((skill, index) => (
+                    <Grid item xs={12} sm={6} md={4} key={index}>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h5" component="div">{skill.name}</Typography>
+                                <Typography variant="body2" color="textSecondary">
                                     {`Nível: ${skill.rating} (${labels[skill.rating]})`}
-                                    </Typography>
-                                    <IconButton onClick={() => handleEditSkill(index, skill)} aria-label="edit">
-                                        <EditIcon />
-                                    </IconButton>
-                                    <IconButton onClick={() => handleDeleteSkill(index)} aria-label="delete">
-                                        <HighlightOffIcon />
-                                    </IconButton>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
-            )}
+                                </Typography>
+                                <IconButton onClick={() => handleEditSkill(index, skill)} aria-label="edit">
+                                    <EditIcon />
+                                </IconButton>
+                                <IconButton onClick={() => handleDeleteSkill(index)} aria-label="delete">
+                                    <HighlightOffIcon />
+                                </IconButton>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
         </Sidebar>
     );
+}
 
 const labels: { [index: number]: string } = {
     0.5: 'Iniciante',
@@ -66,4 +59,4 @@ const labels: { [index: number]: string } = {
     4: 'Avançado',
     4.5: 'Expert',
     5: 'Expert'
-}}
+};
